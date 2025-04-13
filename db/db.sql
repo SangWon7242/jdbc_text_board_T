4182702178936812443751766e2ad9286ddc8082
SangWon7242 <gotkdqja@gmail.com>
2025-04-13 14:37:38 +0900

    insert 테스트 진행

--- db/db.sql
+++ db/db.sql
@@ -6,3 +6,23 @@ CREATE DATABASE text_board;
 
 # 데이터 베이스 선택
 USE text_board;
+
+# article 테이블 생성
+CREATE TABLE article (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	subject CHAR(100) NOT NULL,
+	content TEXT NOT NULL
+);
+
+# 게시물 데이터 추가
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+subject = '제목',
+content = '내용';
+
+# 게시물 데이터 조회
+SELECT *
+FROM article;
