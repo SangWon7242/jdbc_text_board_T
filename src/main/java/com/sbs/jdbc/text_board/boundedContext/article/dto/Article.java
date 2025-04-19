9a9b8dcec0d93d7fa7cdd66b4ba0cd2fdff895bb
SangWon7242 <gotkdqja@gmail.com>
2025-04-19 12:23:38 +0900

    JDBC SELECT 테스트

--- src/main/java/com/sbs/jdbc/text_board/boundedContext/article/dto/Article.java
+++ src/main/java/com/sbs/jdbc/text_board/boundedContext/article/dto/Article.java
@@ -1,7 +1,11 @@
 package com.sbs.jdbc.text_board.boundedContext.article.dto;
 
+import java.time.LocalDateTime;
+
 public class Article {
   private final int id;
+  private LocalDateTime regDate;
+  private LocalDateTime updateDate;
   private String subject;
   private String content;
 
@@ -9,6 +13,26 @@ public class Article {
     this.id = id;
     this.subject = subject;
     this.content = content;
+  }
+
+  public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String subject, String content) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.subject = subject;
+    this.content = content;
+  }
+
+  public LocalDateTime getRegDate() {
+    return regDate;
+  }
+
+  public LocalDateTime getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(LocalDateTime updateDate) {
+    this.updateDate = updateDate;
   }
 
   public int getId() {
@@ -35,6 +59,8 @@ public class Article {
   public String toString() {
     return "Article{" +
         "id=" + id +
+        ", regDate=" + regDate +
+        ", updateDate=" + updateDate +
         ", subject='" + subject + '\'' +
         ", content='" + content + '\'' +
         '}';
