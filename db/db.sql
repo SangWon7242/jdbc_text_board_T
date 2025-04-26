87cbfa34bfcbd628230779f76768b12a3953a201
SangWon7242 <gotkdqja@gmail.com>
2025-04-26 12:10:28 +0900

    테스트 데이터 생성

--- db/db.sql
+++ db/db.sql
@@ -16,12 +16,24 @@ CREATE TABLE article (
 	content TEXT NOT NULL
 );
 
-# 게시물 데이터 추가
+# 게시물 테스트 데이터 추가
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
-subject = '제목',
-content = '내용';
+subject = '제목1',
+content = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+subject = '제목2',
+content = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+subject = '제목3',
+content = '내용3';
 
 # 게시물 데이터 조회
 SELECT *
@@ -37,3 +49,37 @@ DATE_FORMAT(A.updateDate, '%Y-%m-%d %H:%i:%s') AS updateDate,
 A.subject,
 A.content
 FROM article AS A;
+
+# member 테이블 생성
+CREATE TABLE `member` (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	username CHAR(100) NOT NULL UNIQUE,
+	password CHAR(150) NOT NULL,
+	name CHAR(100) NOT NULL
+);
+
+# 회원 테스트 데이터
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+username = 'user1',
+password = '1234',
+name = '양관식';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+username = 'user2',
+password = '1234',
+name = '오애순';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+username = 'user3',
+password = '1234',
+name = '양금명';
+
+SELECT * FROM `member`;
